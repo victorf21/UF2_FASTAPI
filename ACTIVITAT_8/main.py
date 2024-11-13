3180cf235be7aba3df65055a11c22faf09f9b91b
Victor Fernandez <fernandezvictor738@gmail.com>
2024-11-13 20:49:44 +0100

    Comentarios añadidos

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -1,17 +1,19 @@
+# Imports
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
-
+# Modelo para validar datos que recibimos del POST
 class Item(BaseModel):
     name: str
     cognom: str 
     edad: int
     altura: float  
     correo: str 
-    telefono: int | None = None
+    telefono: int | None = None # Opcional
 
 app = FastAPI()
 
+# Ruta GET para obtener los datos de alumne
 @app.get("/alumne")
 async def root():
     return {"nom": "Victor",
@@ -19,13 +21,20 @@ async def root():
             "edad": "20"
             }
 
+# Simulación de diccionario, para simular base de datos
 items = {"foo": "The Foo Wrestlers"}
+
+# Ruta GET para leer un item a partir de un ID
 @app.get("/items/{item_id}")
 async def read_item(item_id: str):
+    # Si el item no existe, lanza error 404
     if item_id not in items:
         raise HTTPException(status_code=404, detail="Item no encontrado, introduce un id ")
+    # Si existe devuelve el valor
     return {"item": items[item_id]}
 
+# Ruta POST para crear un nuevo item
 @app.post("/items/")
 async def create_item(item: Item):
+    # Devuelve lo creado
     return item
