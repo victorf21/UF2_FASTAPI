ad02681e4c901b3ed219b5089bde6bc87bbb7221
Victor Fernandez <fernandezvictor738@gmail.com>
2024-11-20 20:00:48 +0100

    documentacio Body-Nested Models

--- ACTIVITAT_9/main.py
+++ ACTIVITAT_9/main.py
@@ -1,21 +1,35 @@
 from typing import Annotated
 
 from fastapi import Body, FastAPI
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, HttpUrl
 
 app = FastAPI()
 
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
 
-class Item(BaseModel):
+class Item(BaseModel): 
     name: str
     description: str | None = Field(
         default=None, title="The description of the item", max_length=300
     )
     price: float = Field(gt=0, description="The price must be greater than zero")
     tax: float | None = None
+    tags: set[str] = set()
+    images: list[Image] | None = None
 
+class Offer(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    items: list[Item]
 
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
     results = {"item_id": item_id, "item": item}
     return results
+
+@app.post("/offers/")
+async def create_offer(offer: Offer):
+    return offer
