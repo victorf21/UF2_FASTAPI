4d05828d32691ce7a53a50bf30189e2a356c8580
Victor Fernandez <fernandezvictor738@gmail.com>
2024-11-25 19:12:12 +0100

    lo que hice hoy

--- ACTIVITAT_10/insertDATA/csvBD.py
+++ ACTIVITAT_10/insertDATA/csvBD.py
@@ -4,6 +4,6 @@ df = pd.read_csv("ACTIVITAT_10\insertDATA\paraules_temàtica_penjat.csv")
 d = df.to_dict()
 
 for x in range(len(df)):
-    p= df['WORD'][x]
-    t= df['THEME'][x]
+    p = df['WORD'][x]
+    t = df['THEME'][x]
     print(df)
